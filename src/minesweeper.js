c311bc44be4eaf183bb6b0cc40227c97fc8cfd2e
Matthew Goodwin <matthewgoodwin@Matthews-MacBook-Pro-4.local>
2017-07-22 12:39:36 -0500

    Added 'let' for variable 'row' declaration

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -2,7 +2,7 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   let board = [];
   for (let rows = 0; rows < numberOfRows; rows++) {
-    row = [];
+    let row = [];
     for (let columns = 0; columns < numberOfColumns; columns++) {
       row.push(' ');
     }
