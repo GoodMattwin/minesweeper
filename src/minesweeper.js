463d0793e9dc7304e36c5c6401f384acf7bc4331
Matthew Goodwin <matthewgoodwin@Matthews-MacBook-Pro-4.local>
2017-07-21 21:31:05 -0500

    Finished Day 10/11 tasks

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,57 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  for (let i = 0; i<board.length; i++) {
-    console.log(board[i].join(' | '));
+/* Codecademy solution for generatePlayerBoard
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    row = [];
+    for (let columns = 0; columns < numberOfColumns; columns++) {
+      row.push(' ');
+    }
+    board.push(row);
   }
+  return board;
 };
 
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+console.log(generatePlayerBoard(3, 3));
+*/
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    board.push([]);
+    for (let columns = 0; columns < numberOfColumns; columns++) {
+      board[rows].push(' ');
+    }
+  }
+  return board;
+};
 
-//test
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    board.push([]);
+    for (let columns = 0; columns < numberOfColumns; columns++) {
+      board[rows].push(null);
+    }
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    // An important note: The code in your while loop has the potential
+    // to place bombs on top of already existing bombs. This will be
+    // fixed when you learn about control flow.
+  }
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player board: ' + printBoard(playerBoard));
+console.log('Bomb board: ' + printBoard(bombBoard));
