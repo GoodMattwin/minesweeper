a65faf3b9c462d218baf57b894478c6a04859bb5
Matthew Goodwin <matthewgoodwin@Matthews-MacBook-Pro-4.local>
2017-07-26 15:27:15 -0500

    Flip tile and show neighbor bomb count

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -32,6 +32,37 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   return board;
 };
 
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = offset[0] + rowIndex;
+    const neighborColumnIndex = offset[1] + columnIndex;
+    if (neighborRowIndex >= 0
+     && neighborRowIndex < numberOfRows
+     && neighborColumnIndex >= 0
+     && neighborColumnIndex < numberOfColumns) {
+       if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
+};
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== ' ') {
+    console.log('This tile has already been flipped!');
+    return;
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
+    playerBoard[rowIndex][columnIndex] = 'B';
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  }
+};
+
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
 };
@@ -39,7 +70,10 @@ const printBoard = board => {
 let playerBoard = generatePlayerBoard(3, 4);
 let bombBoard = generateBombBoard(3, 4, 5);
 
-console.log('Player board: ');
+console.log('Player board:');
 printBoard(playerBoard);
-console.log('Bomb board: ');
+console.log('Bomb board:');
 printBoard(bombBoard);
+flipTile(playerBoard, bombBoard, 0, 0);
+console.log('Updated Player Board:');
+printBoard(playerBoard);
